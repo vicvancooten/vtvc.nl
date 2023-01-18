2db81bffba1e6954ca0d217fe9e027e4b5e7fdea
Vic van Cooten <vic.van.cooten@a-insights.eu>
2023-01-18 18:48:07 +0100

    Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import { Roboto } from "@next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const roboto = Roboto({
   weight: "400",
@@ -10,7 +11,7 @@ const roboto = Roboto({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={roboto.className} style={{ height: "100%" }}>
-      <Component {...pageProps} />
+      <Component {...pageProps} /> <Analytics />
     </main>
   );
 }
