711d6cf18753fb3573b04a6b35967d30da3bfcce
Vic van Cooten <vic.van.cooten@a-insights.eu>
2023-03-22 10:11:16 +0100

    Fix missing mbid from API

--- src/lib/lastfm.ts
+++ src/lib/lastfm.ts
@@ -35,9 +35,17 @@ export const getScrobbleCount: () => Promise<LastFMResponseType> = cache(
     const weekly = await weekly_response.json()
 
     // Fetch info (images) for AOTW
+    const album = weekly?.weeklyalbumchart?.album?.[0]
+    let aotw_query = `mbid=${album?.mbid}`
+
+    // If for some reason mbid was not provided by the previous request, let's look for it based on artist and album
+    if (!album?.[0]?.mbid) {
+      aotw_query = `artist=${album?.artist?.['#text']}&album=${album?.name}`
+    }
     const aotw_response = await fetch(
-      `https://ws.audioscrobbler.com/2.0/?method=album.getInfo&mbid=${weekly?.weeklyalbumchart?.album?.[0]?.mbid}&&user=Duveaux&api_key=${process.env.LASTFM_API_KEY}&format=json`
+      `https://ws.audioscrobbler.com/2.0/?method=album.getInfo&${aotw_query}&user=Duveaux&api_key=${process.env.LASTFM_API_KEY}&format=json`
     )
+
     const aotw = await aotw_response.json()
 
     // Structure & return
